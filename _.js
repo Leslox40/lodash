4ae78b35ce3b81f2d3e0cd94b22268a8b83933ee
Kamerun <fonepiles40x@gmail.com>
2021-02-06 14:28:51 +0100

    Implementing the inRange() function for the lodash library

--- _.js
+++ _.js
@@ -3,6 +3,24 @@ const _ = {
       const lowerClampedValue = Math.max(number, lower);
       const clampedValue = Math.min(lowerClampedValue, upper);
       return clampedValue;
+    },
+
+    inRange (number, start, end) {
+        //checking to see if no end value was given
+        //end take the start value and start takes 0
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        } else if (start > end) {
+            //swapping if the start is greater end value
+            let temp = end;
+            end = start;
+            start = temp;
+        } 
+        const isInRange = start <= number && number < end;
+        console.log(isInRange);
+
+        return isInRange;
     }
   };
 
